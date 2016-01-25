6eba9c5de68d5c1c4fb900e19f4d02c3f93fd96c
George Giddens <github@georgegiddens.com>
2016-01-25 13:13:15 -0500

    New report #2 query

--- Compliance/ComplianceReports/sql/Compliance Report Number 2.sql
+++ Compliance/ComplianceReports/sql/Compliance Report Number 2.sql
@@ -65,46 +65,28 @@ declare
 	join FormTemplateInputItem ii on c.LayoutControlID = ii.InputAreaId 
 	left join FormTemplateInputItemType iit on ii.TypeId = iit.Id
 	left join MonSubmissionArea msa on c2.LayoutControlID = msa.FormTemplateControlID
-	where c2.LayoutControlID in (
-		'7AA6797C-9F89-462A-B667-34F821F8D325' -- Dates of Meeting (Confirm dates with evidence in the file)
-		, '4249765D-446C-4199-9A4A-1961C6CD81C2' -- Present Levels of Academic Achievement and Functional Performance
-		, 'C4404A98-12EB-4791-8341-8E21FAF7E553' -- Post-School Considerations
-		, 'B66A23BC-4292-4F42-A73B-1025C6A0A8C7' -- Annual Goals/Objectives
-		, '0574F0D4-2A51-41E2-BC1D-DFCFF34CE0A0' -- Accommodations and Modifications
-		, '67373EE2-C726-4E60-864B-AA66EDF9D71F' -- State/District Assessments
-		, 'AEFB23A3-4DAE-4B7E-8C2F-1D620584D814' -- Service Delivery Statement
-		, '367FBEB2-DC15-4E78-B2ED-9CF12D05F59E' -- Recommended Placement in the LRE
-		, '98DA711A-5190-474E-8636-8CD8330C63BC' -- Prior Written Notice
-		, '2498D42E-85DE-484B-B609-E9823B6C99B1' -- Appendix A: Early Childhood IEP
-		, '500F3BAE-FFE7-4876-A2D8-B79DF0D3B300' -- Appendix B: Evaluation/Reevaluation
-	) 
-)
-
-, sectionStatementCount as (
-	select a.SectionID, a.Section, a.SectionSequence, Total = count(*) 
-	from attributesCTE a
-	where a.AttributeType = 'SingleSelect'
-	group by a.SectionID, a.Section, a.SectionSequence
 )
 
 , formDataDetail as (
 	select [Administrative Unit] = ou.Name
-		, a.SectionID
+		, InstanceID = fi.ID
+		--, a.SectionID
 		, a.Section
 		, a.ShortName
 		, a.Statement
-		, a.ReportLabel
-		, a.AttributeID
-		-- One Annual Goals Statement is to be considered with the Post-Secondary data calculation. We are increasing the sequence number to differentiate it. (and the item in the Goal area specific to transition IEPs)
-		, StatementSequence = case when a.Section = '' then 700+a.Sequence else a.Sequence end -- setting the sequence again here..
-		, InstanceID = fi.ID
-		, Yes = case when sfo.ReportLabel = 'Met' then 1 when sfo.ReportLabel = 'Not Applicable' then 1 else 0 end
-		--, ac.SectionCount
-		--, rs.ExcludedDate
+		--, a.ReportLabel
+		--, a.AttributeID
+		, StatementSequence = a.Sequence 
+		, a.IsNote
+		, Value =
+			case 
+				when AttributeType = 'SingleSelect' then convert(varchar(max), sfo.Label) -- note: We can use Label instead of ReportLabel -- ReportLabel = Met, Label = Yes
+				when AttributeType = 'Text' then convert(varchar(max), tv.Value)
+			end
 	from FormInstance fi
 	left join FormInstanceInterval fii on fi.ID = fii.InstanceID
 	left join FormInputValue fiv on fii.ID = fiv.IntervalID
-	join attributesCTE a on fiv.InputFieldId = a.AttributeID -- inner join or else null attribute (text)
+	left join attributesCTE a on fiv.InputFieldId = a.AttributeID -- inner join or else null attribute (text)
 	-- mon
 	left join MonSubmissionRecordForm rf on fii.InstanceID = rf.ID  
 	left join MonSubmissionRecord r on rf.SubmissionRecordID = r.ID
@@ -114,11 +96,11 @@ declare
 	left join RosterYear ry on sub.RosterYearID = ry.ID
 
 	-- single select
-	join FormInputSingleSelectValue ssv on fiv.ID = ssv.ID -- and a.InputItemType = 'SingleSelect' -- inner join, single select only - not nec to check a.inputitemtype
+	left join FormInputSingleSelectValue ssv on fiv.ID = ssv.ID -- and a.InputItemType = 'SingleSelect' -- inner join, single select only - not nec to check a.inputitemtype
 	left join FormTemplateInputSelectFieldOption sfo on ssv.SelectedOptionID = sfo.ID
 
-	---- areaSectionCount
-	--left join areaSectionCount ac on a.SubmissionAreaID = ac.SubmissionAreaID
+	-- text
+	left join FormInputTextValue tv on fiv.Id = tv.Id
 
 	where fi.TemplateID = 'F87DC304-78EB-49DC-AB61-16EBDCA2174C' -- Compliance Checklist
 	and ou.ID in (select OrgUnitID from UserProfileOrgUnit where UserProfileID = @userID)
@@ -126,55 +108,14 @@ declare
 	and rs.ExcludedDate is null
 
 	---- TESTING
- 		--and ou.ID = '3B26E265-39EA-422D-A17C-B8C8D9E312D8'
+ 		and ou.ID = '3B26E265-39EA-422D-A17C-B8C8D9E312D8'
 	---- TESTING
 )
-, AUSampleSize as (
-	select [Administrative Unit] = case when grouping([Administrative Unit]) = 0 then [Administrative Unit] else 'State' end
-		, SortBit = grouping([Administrative Unit])
-		, IEPCount = count(distinct InstanceID) 
-	from formDataDetail
-	group by rollup([Administrative Unit])
-)
-, instanceCounts as (
-	select fdd.[Administrative Unit]
-		, fdd.InstanceID
-		, InstanceYesCount = sum(fdd.Yes) 
-		, SectionCount = max(ac.Total)
-	from formDataDetail fdd
-	left join sectionStatementCount ac on fdd.SectionID = ac.SectionID
-	group by fdd.[Administrative Unit], fdd.InstanceID
-)
-, AUCounts as (
-	select [Administrative Unit] = case when grouping([Administrative Unit]) = 0 then [Administrative Unit] else 'State' end
-		, SectionID
-		, Section
-		, ShortName -- SectionLabel
-		, Statement -- InputItemLabel
-		, ReportLabel
-		, AttributeID
-		, StatementSequence
-		, ItemYesCount = sum(fdd.Yes)
-		, SortBit = grouping([Administrative Unit])
-	from formDataDetail fdd
-	group by rollup([Administrative Unit]), SectionID, Section, ShortName, Statement, StatementSequence, ReportLabel, AttributeID
-)
 
-select *
-from (
-select 
-	Section = case when @sectionID = '97075423-230C-4D80-99C5-C8053D08615B' then 'Post-School Considerations' else ac.Section end
-	, ac.[Administrative Unit]
-	, ac.ShortName
-	--, ReportLabel
-	, Statement
-	, ac.StatementSequence
-	, PerItemPct = cast(round((ac.ItemYesCount/cast(ss.IEPCount as float))*100, 2) as decimal(5,1))
-		--, ss.IEPCount
-		--, ac.ItemYesCount
-	, ac.SortBit
-from AUCounts ac
-left join AUSampleSize ss on ac.[Administrative Unit] = ss.[Administrative Unit]
-) t
-order by SortBit, [Administrative Unit], StatementSequence
+
+select * from formDataDetail 
+where InstanceID = '2B3B183D-FB68-423D-89E7-071F0BB3533D' 
+--where AttributeID = '2096F365-933D-4EE3-80EC-5C29EC0311CD' and Value is not null
+order by InstanceID, StatementSequence
+
 
